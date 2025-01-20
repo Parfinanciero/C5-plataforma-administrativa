2657e21ad27255d305cea0e75c7bce0a2afaeb5d
Njco1 <Nicolasaf17@gmail.com>
2025-01-20 17:24:32 -0500

    Primer commit

---
+++ src/main/java/com/Parfinanciero/ParfinancieroAdmin/ParfinancieroAdminApplication.java
@@ -0,0 +1,13 @@
+package com.Parfinanciero.ParfinancieroAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParfinancieroAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParfinancieroAdminApplication.class, args);
+	}
+
+}
