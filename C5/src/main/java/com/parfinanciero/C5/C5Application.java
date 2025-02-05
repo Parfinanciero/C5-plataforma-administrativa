b019e41829725723b91500e2bfc1f740a2fe09dc
BrayanCondee <soycondebrayan@gmail.com>
2025-02-05 16:46:40 -0500

    feat: .env file added

--- C5/src/main/java/com/parfinanciero/C5/C5Application.java
+++ C5/src/main/java/com/parfinanciero/C5/C5Application.java
@@ -1,15 +1,18 @@
 package com.parfinanciero.C5;
 
 import org.springframework.boot.SpringApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class 	C5Application {
+public class C5Application {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(C5Application.class, args);
 	}
-
 }
